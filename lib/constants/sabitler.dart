419fcae956c67749f368fb183783f38fb9afaabb
seheerst <seherst9@gmail.com>
2022-08-04 16:36:55 +0300

    feat: add color

--- lib/constants/sabitler.dart
+++ lib/constants/sabitler.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class Sabitler {
   static const anaRenk = Color(0xff5c85d6);
   static const ikinciRenk = Color(0xff99b3e6);
+  static const loginArka = Color(0xff1d7296);
 
 }
