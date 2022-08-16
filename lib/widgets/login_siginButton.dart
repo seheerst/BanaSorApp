2e519b2f59ac6f22d3f662c2bdb9abab68b614a7
seheerst <seherst9@gmail.com>
2022-08-16 16:55:12 +0300

    feat: Firebase Collections

--- lib/widgets/login_siginButton.dart
+++ lib/widgets/login_siginButton.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-
 import '../constants/sabitler.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Container fireBaseUIButton(BuildContext context, String title, Function onTap) {
   return Container(
@@ -12,10 +12,29 @@ Container fireBaseUIButton(BuildContext context, String title, Function onTap) {
     child: TextButton(
         onPressed: () {
           onTap();
+
         },
         child: Text(
           title,
           style: const TextStyle(fontSize: 25, color: Sabitler.loginArka),
         )),
   );
+}
+class FirebaseCollections extends StatefulWidget {
+  const FirebaseCollections({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FirebaseCollectionsState createState() => _FirebaseCollectionsState();
+}
+
+class _FirebaseCollectionsState extends State<FirebaseCollections> {
+  final _firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    var userRef = _firestore.doc('User1');
+    debugPrint(userRef.get().toString());
+    return Container();
+  }
 }
