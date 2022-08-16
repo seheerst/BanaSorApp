ea73856f5ac0a4fe4b669595766530aaaa2f7720
seheerst <seherst9@gmail.com>
2022-08-16 16:52:27 +0300

    feat: design arrangement

--- lib/widgets/AyarlarDrawer.dart
+++ lib/widgets/AyarlarDrawer.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: library_private_types_in_public_api, non_constant_identifier_names
 
 import 'package:bana_sor_app/constants/sabitler.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../screens/sifreDegistir.dart';
 
 class AppDrawer extends StatefulWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class AppDrawer extends StatefulWidget {
 }
 
 class _AppDrawerState extends State<AppDrawer> {
+  late FirebaseAuth auth;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,24 +30,32 @@ class _AppDrawerState extends State<AppDrawer> {
         ),
         body: ListView(
           children: [
-            ListTileWidget('Tercihler'),
-            ListTileWidget('Kişisel Bilgiler'),
-            ListTileWidget('e-mail adresimi değiştir'),
-            ListTileWidget('Şifremi değiştir'),
-            ListTileWidget('Kullanım Koşulları'),
-            ListTileWidget('İletişim'),
-            ListTileWidget('Gizlilik Politikası'),
+
+            ListTileWidget('Kişisel Bilgiler', () {}, Icons.person,),
+            ListTileWidget('e-mail adresimi değiştir', () {}, Icons.mail),
+            ListTileWidget('Şifremi değiştir', () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SifreDegistirScreen()));
+            }, Icons.lock),
+            ListTileWidget('Kullanım Koşulları', () {}, Icons.book ),
+            ListTileWidget('İletişim', () {}, Icons.phone),
+            ListTileWidget('Gizlilik Politikası', () {}, Icons.security),
           ],
         ));
   }
 
-  ListTileWidget(String baslik) {
+  ListTileWidget(String baslik, Function onTap, IconData icon) {
     return ListTile(
-      onTap: () {},
+      onTap: () {
+        onTap();
+      },
       title: Text(
         baslik,
         style: const TextStyle(fontSize: 20),
       ),
+      leading: Icon(icon),
       trailing: const Icon(
         Icons.arrow_forward_ios,
         color: Sabitler.anaRenk,
